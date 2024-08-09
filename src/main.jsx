3385991aecb488411147612afac280a4561c7ddc
HelloAyan <helloayan171@gmail.com>
2024-08-09 23:54:33 +0600

    added new template

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,8 @@ import TemplateTest from './TemplateTest.jsx'
 import TemplateTest2 from './TemplateTest2.jsx'
 import Template8 from './Template8.jsx'
 import Template9 from './Template9.jsx'
+import Template10 from './Template10.jsx'
+import Template11 from './Template11.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -22,5 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <TemplateTest2 />
     <Template8 />
     <Template9 />
+    <Template10 />
+    <Template11 />
   </React.StrictMode>,
 )
